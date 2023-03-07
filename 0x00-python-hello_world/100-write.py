e3ba69706ee7b6dc8d689f0f18d69ca20bab9e78
CodeLordGh <babohkossi228@icloud.com>
2023-03-07 08:02:17 -0800

    Advance tasks crushed

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import sys # import the sys module to access the write() function 
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n") # write the string to stderr.
+sys.exit(1) # exit with a status code of 1
---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):#function takes two arguments a & b
+    return (98 + a ** b); # and returns a raised to the power of b plus 98
+#CodeLod
