6f53b8cd2ea4ace501a5375315250ffb8545aca2
CodeLordGh <babohkossi228@icloud.com>
2023-04-11 17:42:37 +0000

    1. My list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
 - The  dir  function returns a list of the names of the attributes and methods of an object. 
 - The list comprehension  [attr for attr in dir(obj)]  generates a list of all the available attributes and methods of the given object. 
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """A custom list class that adds a print_sorted method."""
+    def print_sorted(self):
+        """Print the list, sorted in ascending order."""
+        sorted_list = sorted(self)
+        print(sorted_list)
+        
