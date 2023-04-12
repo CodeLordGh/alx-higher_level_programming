f00f135045d3ffb926944ca731c0d912e25e5b6e
CodeLordGh <babohkossi228@icloud.com>
2023-04-12 13:41:43 +0000

    hey

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,4 @@ class MyList(list):
     """A custom list class that adds a print_sorted method."""
     def print_sorted(self):
         """Print the list, sorted in ascending order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
